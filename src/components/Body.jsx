2aa18514f41be0b688377ef0ada41b6782ede82e
Maria Elena Haydee Ponce <120025349+hechizera10@users.noreply.github.com>
2024-04-28 11:04:46 -0300

    Chatbot_ScrollBar-colors

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import { MDBCardBody, MDBTextArea } from "mdb-react-ui-kit";
 import { BotMessage } from "./BotMessage";
 import { UserMessage } from "./UserMessage";
 import { Input } from "./Input";
+import Data from "../utils/company-config.json";
+
 
 export const Body = () => {
+	const [companyData, setCompanyData] = useState(Data);
+	const colors = companyData["bussiness-data"]["primary-color"]+" "+companyData["bussiness-data"]["secondary-color"];
 	return (
-		<MDBCardBody>
+		<MDBCardBody id="card-body" style={{scrollbarWidth: "thin",
+		scrollbarColor:  colors,}}>
 			<ul className="ps-0 pe-2" id="messages" style={{height:"16rem", overflowY:"scroll"}}>
 				<li className="">
 					<BotMessage />
