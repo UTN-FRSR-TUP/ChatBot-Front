fd0584d272f59d0d9401f3519b00274ca51e614b
Maria Elena Haydee Ponce <120025349+hechizera10@users.noreply.github.com>
2024-04-28 17:36:51 -0300

    Chatbot_Front-Styles-Finished

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MDBCardBody, MDBTextArea } from "mdb-react-ui-kit";
 import { BotMessage } from "./BotMessage";
 import { UserMessage } from "./UserMessage";
@@ -14,50 +14,54 @@ export const Body = ({
 	secondaryColor,
 }) => {
 	const colors = primaryColor + " " + secondaryColor;
-	const [botMessage, setBotMessage] = useState(welcomeMessage);
+
 	const [listMessage, setListMessage] = useState([]);
+
+
+	
 	return (
 		<MDBCardBody
 			id="card-body"
 			style={{ scrollbarWidth: "thin", scrollbarColor: colors }}
 		>
+			
 			<ul
 				className="ps-0 pe-2"
 				id="messages"
 				style={{ height: "calc(75vh - 11rem)", overflowY: "scroll" }}
 			>
 				<BotMessage
-						botImg={botImg}
-						botName={botName}
-						companyName={companyName}
-						secondaryColor={secondaryColor}
-						botMessage={welcomeMessage}
-					/>
-			{listMessage.map((message, index) => {
-				if (message.fromUser) {
-					return (
-						<li key={index} className="">
-							<UserMessage userMessage={message.text} />
-						</li>
-					);
-				} else {
-					return (
-						<li key={index} className="">
-							<BotMessage
-								botImg={botImg}
-								botName={botName}
-								companyName={companyName}
-								welcomeMessage={welcomeMessage}
-								secondaryColor={secondaryColor}
-								botMessage={botMessage}
-							/>
-						</li>
-					);
-				}
-			})}
+					botImg={botImg}
+					botName={botName}
+					companyName={companyName}
+					secondaryColor={secondaryColor}
+					botMessage={welcomeMessage}
+				/>
+				{listMessage.map((message, index) => {
+					if (message.fromUser) {
+						return (
+							<li key={index} className="">
+								<UserMessage userMessage={message.text} />
+							</li>
+						);
+					} else {
+						return (
+							<li key={index} className="">
+								<BotMessage
+									botImg={botImg}
+									botName={botName}
+									companyName={companyName}
+									welcomeMessage={welcomeMessage}
+									secondaryColor={secondaryColor}
+									botMessage={message.Message}
+								/>
+							</li>
+						);
+					}
+				})}
 			</ul>
-			
-			<Input setListMessage={setListMessage}/>
+
+			<Input setListMessage={setListMessage} />
 		</MDBCardBody>
 	);
 };
