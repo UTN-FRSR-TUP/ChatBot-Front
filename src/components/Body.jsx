5957f77a0a22119aa04f1fb4cda300fdec049bde
Maria Elena Haydee Ponce <120025349+hechizera10@users.noreply.github.com>
2024-05-11 12:00:32 -0300

    Fix ScrollBar at End

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,9 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { MDBCardBody } from "mdb-react-ui-kit";
 import { BotMessage } from "./BotMessage";
 import { UserMessage } from "./UserMessage";
 import { Input } from "./Input";
-import { consultaChatBot } from "../utils/api-connect"
-
+import { consultaChatBot } from "../utils/api-connect";
 
 export const Body = ({
 	botImg,
@@ -13,70 +12,66 @@ export const Body = ({
 	welcomeMessage,
 	primaryColor,
 	secondaryColor,
-	widthImg
+	widthImg,
 }) => {
 	const colors = primaryColor + " " + secondaryColor;
 
-	const [listMessage, setListMessage] = useState([{ text: welcomeMessage, fromUser: false }]);
+	const [listMessage, setListMessage] = useState([
+		{ text: welcomeMessage, fromUser: false },
+	]);
+	const messagesEndRef = useRef(null);
 
 	useEffect(() => {
 		const fetchChatBotResponse = async () => {
 			const reversedListMessage = [...listMessage].reverse();
 			if (reversedListMessage[0].fromUser) {
 				try {
-					// Usar async/await para esperar la respuesta de consultaChatBot
-					const respuesta = await new Promise((resolve, reject) => {
-						consultaChatBot(reversedListMessage[0].text)
-							.then(respuesta => resolve(respuesta))
-							.catch(error => reject(error));
-					});
-		
-					// Actualizar el estado solo después de que la respuesta esté disponible
-					setListMessage(prevListMessage => [
+					const respuesta = await consultaChatBot(
+						reversedListMessage[0].text
+					);
+					setListMessage((prevListMessage) => [
 						...prevListMessage,
 						{ text: respuesta, fromUser: false },
 					]);
 				} catch (error) {
-					console.error('Error consultando al chatbot:', error);
+					console.error("Error consultando al chatbot:", error);
 				}
 			}
 		};
-		
+
 		fetchChatBotResponse();
 	}, [listMessage]);
 
-
-
-
+	useEffect(() => {
+		if (messagesEndRef.current) {
+			messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+		}
+	}, [listMessage]);
 
 	return (
 		<MDBCardBody
 			id="card-body"
-			style={{ scrollbarWidth: "thin", scrollbarColor: colors, zIndex: "10", }}
+			style={{
+				scrollbarWidth: "thin",
+				scrollbarColor: colors,
+				zIndex: "10",
+			}}
 		>
-
 			<ul
 				className="ps-0 pe-2"
 				id="messages"
 				style={{ height: "calc(75vh - 11rem)", overflowY: "scroll" }}
 			>
-				{/* <BotMessage
-					botImg={botImg}
-					botName={botName}
-					companyName={companyName}
-					secondaryColor={secondaryColor}
-					botMessage={welcomeMessage}
-				/> */}
 				{listMessage.map((message, index) => {
 					if (message.fromUser) {
 						return (
-							<li key={index} className="">
+							<li key={index}>
 								<UserMessage userMessage={message.text} />
 							</li>
 						);
 					} else {
 						return (
-							<li key={index} className="">
+							<li key={index}>
 								<BotMessage
 									botImg={botImg}
 									botName={botName}
@@ -89,8 +84,8 @@ export const Body = ({
 						);
 					}
 				})}
+				<div ref={messagesEndRef} />
 			</ul>
-
 			<Input setListMessage={setListMessage} />
 		</MDBCardBody>
 	);
